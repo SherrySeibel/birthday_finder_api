f056b4777111a044ccbd4d95c19468d003b45841
Sherry Seibel <sherry.seibel@gmail.com>
2015-02-02 11:45:12 -0500

    Add devise for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 require "api_constraint"
 
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: :json },
     constraints: { subdomain: :api }, path: "/"  do
     scope module: :v1,
       constraints: ApiConstraint.new(version: 1, default: true) do
+      resources :users, only: [:show, :create, :update, :destroy]
       resources :people, only: [:show, :create, :update, :destroy]
     end
   end
